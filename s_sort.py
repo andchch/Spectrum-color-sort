0f2c5a96486f495b1ab463e1d299e38a0bfb5592
Мартинич Андрей <aimartinich@edu.hse.ru>
2022-10-06 18:53:56 +0300

    Реализовано сохранение отсортированных изображений

--- s_sort.py
+++ s_sort.py
@@ -1,6 +1,7 @@
-import os
-import math
 import argparse
+import math
+import os
+
 from PIL import Image
 
 parser = argparse.ArgumentParser()
@@ -14,6 +15,13 @@ if not os.path.exists(args.outdir):
 
 def color_diff(c1, c2):
     return math.sqrt((c1[0] - c2[0]) ** 2 + (c1[1] - c2[1]) ** 2 + (c1[2] - c2[2]) ** 2)
+
+
+def sort(c_list, base_color):
+    print('\nSorting by color {}'.format(base_color))
+    for key in sorted(c_list.keys(), key=lambda k: color_diff(c_list[k], base_color)):
+        print('{}: {}'.format(key, c_list[key]))
+    return sorted(c_list.items(), key=lambda x: color_diff(x[1], base_color))
 
 
 new_size = (1, 1)
@@ -37,6 +45,7 @@ blue_list = {}
 violet_list = {}
 
 s_colors = [red, orange, yellow, green, cyan, blue, violet]
+colors_names = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'violet']
 img_lists = [red_list, orange_list, yellow_list, green_list, cyan_list, blue_list, violet_list]
 
 for filename in os.listdir(args.indir):
@@ -57,10 +66,14 @@ for filename in os.listdir(args.indir):
         print('Closest color: {}'.format(s_colors[min_index]))
         img_lists[min_index][file] = rgb
 
-
-def sort(c_list, base_color):
-    print('\nSorting by color {}'.format(base_color))
-    for key in sorted(c_list.keys(), key=lambda k: color_diff(c_list[k], base_color)):
-        print('{}: {}'.format(key, c_list[key]))
-    return sorted(c_list.items(), key=lambda x: color_diff(x[1], base_color))
-
+for i, color in enumerate(s_colors):
+    j = 0
+    img_lists[i] = sort(img_lists[i], color)
+    print('\nColor: {}'.format(color))
+    for file, rgb in img_lists[i]:
+        print('File: {}'.format(file))
+        print('RGB: {}'.format(rgb))
+        img = Image.open(file)
+        img.save(
+            os.path.join(args.outdir, str(i) + '_' + str(j) + '_' + colors_names[i] + '_' + os.path.basename(file)))
+        j += 1
