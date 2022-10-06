2494164c214b42e864c2679e237d677fe16716dd
Мартинич Андрей <aimartinich@edu.hse.ru>
2022-10-06 19:39:05 +0300

    Теперь промежуточная информация выводится в отдельный файл mylog.log. Добавлена обработка ошибки "Директория с изображениями не найдена". Добавлен выход из программы в случае отсутствия изображений в директории с информированием пользователя об этом. Обновлен .gitignore

--- s_sort.py
+++ s_sort.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import math
 import os
 
@@ -9,8 +10,8 @@ parser.add_argument('-indir', type=str, default='test-dataset', help='Input dire
 parser.add_argument('-outdir', type=str, default='output', help='Output directory')
 args = parser.parse_args()
 
-if not os.path.exists(args.outdir):
-    os.makedirs(args.outdir)
+logging.basicConfig(filename="mylog.log", filemode='w', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 def color_diff(c1, c2):
@@ -18,9 +19,9 @@ def color_diff(c1, c2):
 
 
 def sort(c_list, base_color):
-    print('\nSorting by color {}'.format(base_color))
+    logging.info('\nSorting by color {}'.format(base_color))
     for key in sorted(c_list.keys(), key=lambda k: color_diff(c_list[k], base_color)):
-        print('{}: {}'.format(key, c_list[key]))
+        logging.info('{}: {}'.format(key, c_list[key]))
     return sorted(c_list.items(), key=lambda x: color_diff(x[1], base_color))
 
 
@@ -47,33 +48,50 @@ violet_list = {}
 s_colors = [red, orange, yellow, green, cyan, blue, violet]
 colors_names = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'violet']
 img_lists = [red_list, orange_list, yellow_list, green_list, cyan_list, blue_list, violet_list]
+try:
+    if len(os.listdir(args.indir)) == 0:
+        logging.error('No images in input directory')
+        print('No images in input directory')
+        exit(1)
+    for filename in os.listdir(args.indir):
+        file = os.path.join(args.indir, filename)
+        if os.path.isfile(file):
+            logging.info('\nProcessing {}'.format(file))
+            img = Image.open(file)
+            rgb = img.resize(new_size).getpixel((0, 0))
+            logging.info('RGB: {}'.format(rgb))
 
-for filename in os.listdir(args.indir):
-    file = os.path.join(args.indir, filename)
-    if os.path.isfile(file):
-        print('\nProcessing {}'.format(file))
-        img = Image.open(file)
-        rgb = img.resize(new_size).getpixel((0, 0))
-        print('RGB: {}'.format(rgb))
+            min_diff = 999999999
+            min_index = 0
+            for i, color in enumerate(s_colors):
+                diff = color_diff(rgb, color)
+                if diff < min_diff:
+                    min_diff = diff
+                    min_index = i
+            logging.info('Closest color: {}'.format(s_colors[min_index]))
+            img_lists[min_index][file] = rgb
+except FileNotFoundError:
+    logging.error('Input directory not found')
+    print('Input directory does not exist. Create \'test-dataset\' directory and put images there'
+          ' or use -indir argument.')
+    exit(1)
 
-        min_diff = 999999999
-        min_index = 0
-        for i, color in enumerate(s_colors):
-            diff = color_diff(rgb, color)
-            if diff < min_diff:
-                min_diff = diff
-                min_index = i
-        print('Closest color: {}'.format(s_colors[min_index]))
-        img_lists[min_index][file] = rgb
-
+print('Sorted images:')
 for i, color in enumerate(s_colors):
+    if not os.path.exists(args.outdir):
+        os.makedirs(args.outdir)
+
     j = 0
     img_lists[i] = sort(img_lists[i], color)
+    logging.info('\nColor: {}'.format(color))
     print('\nColor: {}'.format(color))
     for file, rgb in img_lists[i]:
+        logging.info('File: {}'.format(file))
         print('File: {}'.format(file))
+        logging.info('RGB: {}'.format(rgb))
         print('RGB: {}'.format(rgb))
         img = Image.open(file)
         img.save(
             os.path.join(args.outdir, str(i) + '_' + str(j) + '_' + colors_names[i] + '_' + os.path.basename(file)))
         j += 1
+logging.info('\nDone')
